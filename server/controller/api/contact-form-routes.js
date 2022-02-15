2ffd27242631f85e80594b24f5cae30598855b72
jasmineakbari <jasmineakbari@yahoo.com>
2022-02-14 23:31:21 -0500

    added nodemailer: function still needs body info before moving to route

--- server/controller/api/contact-form-routes.js
+++ server/controller/api/contact-form-routes.js
@@ -4,7 +4,7 @@ const email_service = require('../../utils/email_service')
 // client side contact form submissions
 router.post('/contact-form', (req, res) => {
     const contact_request = req.body;
-
+    
 });
 
 module.exports = router;
---
+++ server/utils/email_service.js
@@ -0,0 +1,33 @@
+const nodemailer = require("nodemailer");
+
+// contact_form email sent via request input
+async function send_contact_email(input) {
+    let email_req = input;
+    const to_email = process.env.CONTACT_ME_EMAIL
+    const auth = process.env.EMAIL_AUTH
+
+    // create reusable transporter object using the default SMTP transport
+    const smtpConfig = {
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, // use SSL
+        auth: {
+            user: to_email,
+            pass:  auth
+        }
+    };
+
+    const transporter = nodemailer.createTransport(smtpConfig);
+
+    // send mail with defined transport object
+    let email_data = await transporter.sendMail({
+        from: to_email,
+        to: to_email, 
+        subject: `New Portfolio Contact Request from ${email_req.contact_name}`,
+        html: ""
+    });
+
+    console.log("Contact request sent!", email_data.messageId);
+}
+
+module.exports = send_contact_email(input);
